7bea8990461588dadfec4e7636315a1661a588a1
yarek <yarek2003@mail.ru>
2023-02-14 21:29:47 +0300

    HW8

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+    task1 ();
+
+    }
+    public static void task1 () {
+        int[] threeInt = new int[] {1,2,3};
+        double[] threeDouble = {1.57, 7.654, 9.986};
+        boolean[] arrBoolean = {true, true, true, false};
+    }
+
+}
